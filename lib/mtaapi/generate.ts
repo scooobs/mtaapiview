6b76ab26245a568fab923f713043684f4425181e
Conal O'Leary <olearyconal01@gmail.com>
2026-09-13 21:50:11 -0400

    fix up

--- lib/mtaapi/generate.ts
+++ lib/mtaapi/generate.ts
@@ -1,21 +1,21 @@
-import _ from "lodash";
-import { invariant } from "../utils/invariant";
-import { createStop } from "./stops";
-import { createTrip } from "./trips";
+import _ from "lodash"
+import { invariant } from "@/lib/invariant"
+import { createStop } from "./stops"
+import { createTrip } from "./trips"
 
-const STATIC_GTFS_SUBWAY_DIR = "static/gtfs_subway";
-const GEN_OUTPUT_DIR = "src/gen/types";
+const STATIC_GTFS_SUBWAY_DIR = "static/gtfs_subway"
+const GEN_OUTPUT_DIR = "src/gen/types"
 
 /* --------------------- STOPS --------------------- */
 {
-  const STOPS_INPUT = "stops.txt";
-  const STOPS_OUTPUT = "stops.ts";
+  const STOPS_INPUT = "stops.txt"
+  const STOPS_OUTPUT = "stops.ts"
 
-  const stopsInputFile = Bun.file(`${STATIC_GTFS_SUBWAY_DIR}/${STOPS_INPUT}`);
-  const contents = await stopsInputFile.text();
-  const [_header, ...lines] = contents.trim().split("\n");
+  const stopsInputFile = Bun.file(`${STATIC_GTFS_SUBWAY_DIR}/${STOPS_INPUT}`)
+  const contents = await stopsInputFile.text()
+  const [_header, ...lines] = contents.trim().split("\n")
 
-  const stops = lines.map(createStop);
+  const stops = lines.map(createStop)
   const out = `export const STOPS = [
 ${stops
   .map(
@@ -23,29 +23,29 @@ ${stops
     stopId: "${stopId}",
     stopName: "${stopName}",
     parentStation: ${parentStation != null ? `"${parentStation}"` : "undefined"}
-}`,
+}`
   )
   .join(",\n")}
-] as const;`;
+] as const;`
 
   invariant(
     _.uniqBy(stops, (s) => s.stopId).length === stops.length,
-    "There were duplicate stopIds",
-  );
-  const stopsOutputFile = Bun.file(`${GEN_OUTPUT_DIR}/${STOPS_OUTPUT}`);
-  stopsOutputFile.write(out);
+    "There were duplicate stopIds"
+  )
+  const stopsOutputFile = Bun.file(`${GEN_OUTPUT_DIR}/${STOPS_OUTPUT}`)
+  stopsOutputFile.write(out)
 }
 
 /* --------------------- TRIPS --------------------- */
 {
-  const TRIPS_INPUT = "trips.txt";
-  const TRIPS_OUTPUT = "trips.ts";
+  const TRIPS_INPUT = "trips.txt"
+  const TRIPS_OUTPUT = "trips.ts"
 
-  const tripsInputFile = Bun.file(`${STATIC_GTFS_SUBWAY_DIR}/${TRIPS_INPUT}`);
-  const contents = await tripsInputFile.text();
-  const [_header, ...lines] = contents.trim().split("\n");
+  const tripsInputFile = Bun.file(`${STATIC_GTFS_SUBWAY_DIR}/${TRIPS_INPUT}`)
+  const contents = await tripsInputFile.text()
+  const [_header, ...lines] = contents.trim().split("\n")
 
-  const trips = lines.map(createTrip);
+  const trips = lines.map(createTrip)
   const out = `export const TRIPS = [
 ${trips
   .map(
@@ -65,16 +65,16 @@ ${trips
     tripHeadsign: "${tripHeadsign}",
     directionId: "${directionId}",
     shapeId: "${shapeId}"
-}`,
+}`
   )
   .join(",\n")}
-]`;
+]`
 
   invariant(
     _.uniqBy(trips, (s) => s.tripId).length === trips.length,
-    "There were duplicate tripIds",
-  );
+    "There were duplicate tripIds"
+  )
 
-  const stopsOutputFile = Bun.file(`${GEN_OUTPUT_DIR}/${TRIPS_OUTPUT}`);
-  stopsOutputFile.write(out);
+  const stopsOutputFile = Bun.file(`${GEN_OUTPUT_DIR}/${TRIPS_OUTPUT}`)
+  stopsOutputFile.write(out)
 }
--- lib/mtaapi/trips.ts
+++ lib/mtaapi/trips.ts
@@ -1,33 +1,33 @@
-import { invariant } from "../utils/invariant";
+import { invariant } from "@/lib/invariant"
 
 // route_id,trip_id,service_id,trip_headsign,direction_id,shape_id
 export interface Trip {
-  routeId: string;
-  tripId: string;
-  queryableTripId: string; // the trip id as represented in a Stop Time Update
-  serviceId: string;
-  tripHeadsign: string;
-  directionId: string;
-  shapeId: string | undefined;
+  routeId: string
+  tripId: string
+  queryableTripId: string // the trip id as represented in a Stop Time Update
+  serviceId: string
+  tripHeadsign: string
+  directionId: string
+  shapeId: string | undefined
 }
 
-const TRIP_ID_REGEX = /_(\d\d\d\d\d\d_\w[\w|\.]+\w)\w\w\w$/;
+const TRIP_ID_REGEX = /_(\d\d\d\d\d\d_\w[\w|\.]+\w)\w\w\w$/
 
 export function createTrip(line: string): Trip {
   const [routeId, tripId, serviceId, tripHeadsign, directionId, shapeId] =
-    line.split(",");
+    line.split(",")
   invariant(
-    routeId && tripId && serviceId && tripHeadsign && directionId && shapeId,
-  );
+    routeId && tripId && serviceId && tripHeadsign && directionId && shapeId
+  )
 
-  const match = tripId.match(TRIP_ID_REGEX);
-  invariant(match);
+  const match = tripId.match(TRIP_ID_REGEX)
+  invariant(match)
   // The Queryable Trip ID is not unique, and should be used
   // as a quick index into all trips. It is not guaranteed that
   // a realtime Trip ID will match a static Trip ID and partial
   // matches are treated as appropriate.
-  const queryableTripId = match[1];
-  invariant(queryableTripId);
+  const queryableTripId = match[1]
+  invariant(queryableTripId)
 
   return {
     routeId,
@@ -37,5 +37,5 @@ export function createTrip(line: string): Trip {
     directionId,
     shapeId,
     queryableTripId,
-  };
+  }
 }
