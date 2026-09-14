9174e3e0c8e2dd71598c36195352dbb146f3a11e
Conal O'Leary <olearyconal01@gmail.com>
2026-09-13 21:51:08 -0400

    hadn't saved lol

--- lib/mtaapi/stops.ts
+++ lib/mtaapi/stops.ts
@@ -1,22 +1,22 @@
-import { invariant } from "../utils/invariant";
+import { invariant } from "@/lib/invariant"
 
 // stop_id,stop_name,stop_lat,stop_lon,location_type,parent_station
 export interface Stop {
-  stopId: string;
-  stopName: string;
-  stopLat: number;
-  stopLon: number;
-  locationType: string | undefined;
-  parentStation: string | undefined;
+  stopId: string
+  stopName: string
+  stopLat: number
+  stopLon: number
+  locationType: string | undefined
+  parentStation: string | undefined
 }
 
 export function createStop(line: string): Stop {
   const [stopId, stopName, stopLat, stopLon, locationType, parentStation] =
-    line.split(",");
+    line.split(",")
   invariant(
     stopId && stopName && stopLat && stopLon,
-    "Required stop fields were not all there",
-  );
+    "Required stop fields were not all there"
+  )
   return {
     stopId,
     stopName,
@@ -24,8 +24,7 @@ export function createStop(line: string): Stop {
     stopLon: parseFloat(stopLat),
     locationType: blank(locationType),
     parentStation: blank(parentStation),
-  };
+  }
 }
 
-const blank = (val: string | undefined) =>
-  val?.length === 0 ? undefined : val;
+const blank = (val: string | undefined) => (val?.length === 0 ? undefined : val)
